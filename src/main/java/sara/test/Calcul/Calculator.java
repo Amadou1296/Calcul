f1b6b234fac7a386d5dd855987f141e9f2d00497
Amadou1296 <saradiallo44@gmail.com>
2020-11-28 12:52:10 +0000

    das

---
+++ src/main/java/sara/test/Calcul/Calculator.java
@@ -0,0 +1,59 @@
+package sara.test.Calcul;
+
+public class Calculator {
+	
+	// addition
+	public int sum(int a,int b)
+	{
+		return a+b;
+	}
+	
+	// soustraction
+	public int minus(int a, int b) {
+		return a - b;
+	}
+
+	// division
+	public int divide(int a, int b) {
+		return a / b;
+	}
+
+	/**
+	 *  : retourne le plus petit élément d’une liste
+	 * @param list
+	 * @return
+	 */
+	public int minElement(int[] list)
+	{
+		int min=list[list.length-1];
+		for(int i=list.length-1;i>1;i--)
+		{
+
+			if(min>list[i-1])
+			{
+				min=list[i-1];
+			}
+		}
+		return min;
+	}
+
+
+	/**
+	 *  : retourne le plus grand élément d’une liste
+	 * @param list
+	 * @return
+	 */
+	public int maxElement(int[] list)
+	{
+		int max=list[list.length-1];
+		for(int i=list.length-1;i>1;i--)
+		{
+
+			if(max<list[i-1])
+			{
+				max=list[i-1];
+			}
+		}
+		return max;
+	}
+}
